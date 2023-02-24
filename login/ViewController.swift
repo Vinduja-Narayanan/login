bf5817df3d2e7aec7e7a5aa35b0b1131ea6cf0c7
vinduja <vindujasuresh3@gmail.com>
2023-02-24 14:17:14 +0530

    Initial Commit

---
+++ login/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Intern IOS on 24/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tf2: UITextField!
+    @IBOutlet weak var tf1: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func button(_ sender: Any) {
+    }
+}
+
